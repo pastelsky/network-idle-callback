ba8f72a5651a3db529a0f9d08f030f499bd9da43
Shubham Kanodia <shubhamsizzles@gmail.com>
2018-03-16 15:13:19 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,32 @@
+importScripts('https://unpkg.com/network-idle-callback/lib/index-serviceworker.js')
+
+self.addEventListener('install', function (e) {
+  console.log('[ServiceWorker] Installed');
+  event.waitUntil(self.skipWaiting())
+});
+
+self.addEventListener('activate', function (e) {
+  console.log('[ServiceWorker] Activated');
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (e) {
+  //console.log('[ServiceWorker] Fetch', e.request.url);
+  self.requestMonitor.add(e)
+  if (!e.clientId) {
+    console.log('was page request')
+  }
+
+  const promise = fetch(e.request)
+    .then((response) => {
+      //console.log('done', e.clientId)
+      self.requestMonitor.remove(e)
+      return response
+    })
+    .catch((e) => {
+      console.log('error')
+      self.requestMonitor.remove(e)
+    })
+
+  e.respondWith(promise)
+});
